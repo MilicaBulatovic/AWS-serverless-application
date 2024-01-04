8b440a66c138f8d40678f8a41c414d30bfc64b26
Milica Bulatovic <99182237+MilicaBulatovic@users.noreply.github.com>
2024-01-04 15:13:01 +0100

    Add files via upload

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-3_84rPHogbs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4rl54isme33qfe6p5354hm3pp7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://hivitq4vb3.execute-api.eu-west-3.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
